aac773b90e5aac9b5223cd37c8de88d32042469c
petrspelos <petrspelos@users.noreply.github.com>
2023-08-11 20:55:48 +0200

    Attack with B button

--- bomb.gd
+++ bomb.gd
@@ -2,9 +2,6 @@ extends RigidBody2D
 
 func _ready():
 	$Timer.connect("timeout", _on_timer_finished);
-	var force_direction = Vector2.RIGHT  # Change this to the direction you want
-	var force_amount = 300  # Change this to the amount of force you want
-	apply_central_impulse(force_direction.normalized() * force_amount)
 
 func _on_timer_finished():
 	var bodies = $ExplosionRadius.get_overlapping_bodies()
