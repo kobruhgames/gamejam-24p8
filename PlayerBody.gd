ca1fcc84c79f5e4c2c6a61df033811fea56c3ee2
petrspelos <petrspelos@users.noreply.github.com>
2023-08-12 12:03:31 +0200

    integrate jump animation frames

--- PlayerBody.gd
+++ PlayerBody.gd
@@ -10,6 +10,7 @@ extends CharacterBody2D
 const BOMB_OFFSET = Vector2(50, 0)
 const BOMB_IMPULSE = 350
 const HORIZONTAL_DIR_STOP = 0
+const PEAK_FRAME_DURATION = 100
 
 const Bomb = preload("res://bomb.tscn")
 var last_direction = 0;
@@ -34,7 +35,7 @@ func _ready():
 func _physics_process(delta):
 	_apply_gravity(delta)
 	var input_horizontal_direction = _read_input_horizontal_direction()
-	_update_animation(input_horizontal_direction)
+	_update_animation(input_horizontal_direction, delta)
 	if input_horizontal_direction == HORIZONTAL_DIR_STOP:
 		_apply_stop_force()
 	else:
@@ -51,15 +52,24 @@ func _read_input_horizontal_direction():
 	last_direction = new_direction if new_direction != HORIZONTAL_DIR_STOP else last_direction 
 	return new_direction
 
-func _update_animation(horizontal_direction):
-	if horizontal_direction == HORIZONTAL_DIR_STOP:
-		if $IdleAnimationTimer.is_stopped() == false:
-			$AnimatedSprite2D.stop() # animation stop
+func _update_animation(horizontal_direction, delta):
+	var non_g_velocity = velocity.y - gravity * delta;
+	if is_on_floor() == false && velocity.y < -PEAK_FRAME_DURATION:
+		$AnimatedSprite2D.play("jump_up_right")
+	elif is_on_floor() == false && velocity.y > PEAK_FRAME_DURATION:
+		$AnimatedSprite2D.play("jump_down_right")
+	elif is_on_floor() == false && velocity.y > -PEAK_FRAME_DURATION && velocity.y < PEAK_FRAME_DURATION:
+		$AnimatedSprite2D.play("jump_peak_right")
 	else:
-		$IdleAnimationTimer.stop()
-		$IdleAnimationTimer.start()
-		$AnimatedSprite2D.play("walk_right")
-		$AnimatedSprite2D.flip_h = horizontal_direction < HORIZONTAL_DIR_STOP
+		if horizontal_direction == HORIZONTAL_DIR_STOP:
+			if $IdleAnimationTimer.is_stopped() == false:
+				$AnimatedSprite2D.play("walk_right")
+				$AnimatedSprite2D.stop() # animation stop
+		else:
+			$IdleAnimationTimer.stop()
+			$IdleAnimationTimer.start()
+			$AnimatedSprite2D.play("walk_right")
+			$AnimatedSprite2D.flip_h = horizontal_direction < HORIZONTAL_DIR_STOP
 
 func _apply_stop_force():
 	velocity.x = lerp(velocity.x, 0.0, friction)
