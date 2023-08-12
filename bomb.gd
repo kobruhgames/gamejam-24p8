7b0478001bfb27b30817b81bbfd6f1549b72f3e4
petrspelos <petrspelos@users.noreply.github.com>
2023-08-12 14:46:01 +0200

    add particle system for explosion

--- bomb.gd
+++ bomb.gd
@@ -4,13 +4,14 @@ func _ready():
 	$Timer.connect("timeout", _on_timer_finished);
 
 func _on_timer_finished():
+	$Particles.emitting = true
 	var bodies = $ExplosionRadius.get_overlapping_bodies()
 	for body in bodies:
 		if body.has_method("receive_damage"):
 			body.receive_damage(1)
+	await get_tree().create_timer(0.2).timeout
 	queue_free()
 
 func _physics_process(delta):
 	var time_left_percentage = $Timer.get_time_left() / $Timer.get_wait_time()
 	$Sprite2D.modulate = Color(1 - time_left_percentage, time_left_percentage, time_left_percentage)
-	$Sprite2D.scale = Vector2(1 - time_left_percentage + 1, 1 - time_left_percentage + 1)
