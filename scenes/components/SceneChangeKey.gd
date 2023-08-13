d01662e279fe6855d67ff215cb1700350077fb03
petrspelos <petrspelos@users.noreply.github.com>
2023-08-13 22:45:15 +0200

    fix teleport collision

--- scenes/components/SceneChangeKey.gd
+++ scenes/components/SceneChangeKey.gd
@@ -4,7 +4,8 @@ extends Node2D
 @export var ExitsGame = false
 
 func _on_area_2d_body_entered(body):
-	if ExitsGame:
-		get_tree().quit()
-	else:
-		get_tree().change_scene_to_packed(NextScene)
+	if body.has_method("receive_enemy_damage"):
+		if ExitsGame:
+			get_tree().quit()
+		else:
+			get_tree().change_scene_to_packed(NextScene)
