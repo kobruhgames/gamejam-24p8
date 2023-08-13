c8d49a40b4f006e19e24a543145ae492f81abe29
petrspelos <petrspelos@users.noreply.github.com>
2023-08-13 19:12:36 +0200

    connect player health

--- scenes/components/PlayerStart.gd
+++ scenes/components/PlayerStart.gd
@@ -1,13 +1,6 @@
 extends Node2D
 
-
 func _ready():
 	var joysticks = Input.get_connected_joypads()
 	if joysticks.size() == 0:
 		$Player2.queue_free()
-		print(joysticks)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
